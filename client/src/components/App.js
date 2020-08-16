a1e4a3d9454690e9fa0fe77ff870447079c71620
Tim Goens <timcgoens@gmail.com>
2020-08-16 16:22:28 -0500

    can call the mongodb and see all of his texts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
+  const [text, setText] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:8080/tweets/")
+  //     .then((response) => setText(response.data));
+  // }, []);
+
+  const tweets = text.map((item) => {
+    return <p key={item._id}>{item.text}</p>;
+  });
+
+  const onSubmitTerm = () => {
+    axios
+      .get(`http://localhost:8080/tweets/word/${searchTerm}`)
+      .then((response) => console.log(response));
+  };
+
   return (
     <div>
-      <h1>Tweetzzzz</h1>
+      <input
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}></input>
+      <button onClick={onSubmitTerm}>Submit</button>
+      <h1>{tweets}</h1>
     </div>
   );
 };
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const tweet = require("../models/tweet");
-const kitten = require("../models/kitten");
 
 const mongoose = require("mongoose");
 
@@ -9,14 +8,29 @@ const db = mongoose.connection;
 db.on("error", (err) => console.log(err));
 db.once("open", () => console.log("connected to database"));
 
-router.get("/", async (req, res) => {
-  tweet.find({}, function (err, result) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(result);
-    }
-  });
+// router.get("/", async (req, res) => {
+//   tweet
+//     .find({ text: { $regex: ".*America*." } }, function (err, result) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.json(result);
+//       }
+//     })
+//     .select("text");
+// });
+
+router.get("/word/:id", async (req, res) => {
+  let searchTerm = req.params.searchTerm;
+  tweet
+    .find({ text: { $regex: `.*${searchTerm}*.` } }, function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(result);
+      }
+    })
+    .select("text");
 });
 
 module.exports = router;
